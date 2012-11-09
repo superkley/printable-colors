9c1e91da89bdcbdb552b1665d013c8c243ba2819
miaomiaokatze@gmail.com <miaomiaokatze@gmail.com@bd37df88-c89a-5d66-e1f4-21d090489b4d>
2012-11-09 13:00:57 +0000

    initial checkin

---
+++ trunk/src/cn/kk/printablecolors/Main.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2012 Xiaoyun Zhu
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package cn.kk.printablecolors;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JColorChooser;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * Demo class
+ */
+public class Main extends JPanel {
+    private static final long serialVersionUID = -7485098391743276874L;
+
+    class ColorTracker implements ActionListener {
+        JColorChooser chooser;
+        Color color;
+
+        public ColorTracker(JColorChooser c) {
+            this.chooser = c;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            this.color = this.chooser.getColor();
+        }
+
+        public Color getColor() {
+            return this.color;
+        }
+    }
+
+    private static final int PADDING = 6;
+
+    public static void main(String[] arguments) throws IOException {
+        JFrame frame = new JFrame("Printable Color Test");
+        frame.setIconImage(ImageIO.read(Main.class.getResource("/print.png")));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Main finder = new Main();
+        frame.setContentPane(finder);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+        finder.chooseColor();
+    }
+
+    ColorInfoPanel actualColorPanel;
+
+    ColorInfoPanel printableColorPanel;
+
+    public Main() {
+        setBackground(new Color(45, 45, 45));
+        GridLayout lyt = new GridLayout(1, 2, PADDING, PADDING);
+        setLayout(lyt);
+        setPreferredSize(new Dimension(600, 300));
+        this.actualColorPanel = new ColorInfoPanel("actual", new Color(255, 255, 0));
+        this.printableColorPanel = new ColorInfoPanel("printable",
+                PrintableColors.findPrintableColor(this.actualColorPanel.getColor()));
+        add(this.actualColorPanel);
+        add(this.printableColorPanel);
+    }
+
+    private void chooseColor() {
+        final JColorChooser chooser = new JColorChooser(this.actualColorPanel.getColor());
+        chooser.getSelectionModel().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                updateColor(chooser.getColor());
+            }
+        });
+
+        final ColorTracker okListener = new ColorTracker(chooser);
+        final JDialog dialog = JColorChooser.createDialog(this, "Choose Color", true, chooser,
+                okListener, null);
+        dialog.setVisible(true);
+
+        final Color c = okListener.getColor();
+        updateColor(c);
+    }
+
+    @Override
+    public Insets getInsets() {
+        return new Insets(PADDING, PADDING, PADDING, PADDING);
+    }
+
+    protected void updateColor(final Color c) {
+        if (c != null) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    Main.this.actualColorPanel.setColor(c);
+                    Main.this.printableColorPanel.setColor(PrintableColors.findPrintableColor(c));
+                    Main.this.actualColorPanel.repaint();
+                    Main.this.printableColorPanel.repaint();
+                }
+            });
+        }
+    }
+}
---
+++ trunk/src/cn/kk/printablecolors/PrintableColors.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2012 Xiaoyun Zhu
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package cn.kk.printablecolors;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Helper class to find printable colors.
+ */
+public final class PrintableColors {
+    private static final List<Color> PRESETS;
+    static {
+        PRESETS = new ArrayList<Color>();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    PrintableColors.class.getResourceAsStream("/presets.res"), Charset.forName("UTF-8")));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Color c = readColor(line);
+                if (c != null) {
+                    PRESETS.add(c);
+                }
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Unable to find the 'presets.res' file in the classpath!");
+            e.printStackTrace();
+        }
+    }
+
+    public static Color findPrintableColor(Color color) {
+        final int[] rgb = { color.getRed(), color.getGreen(), color.getBlue() };
+        final float[] hsb = Color.RGBtoHSB(rgb[0], rgb[1], rgb[2], new float[3]);
+
+        // System.out.println("rgb: " + Arrays.toString(rgb));
+        // System.out.println("hsb: " + Arrays.toString(hsb));
+        if (rgb[0] == rgb[1] && rgb[1] == rgb[2]) {
+            return Color.getHSBColor(hsb[0], hsb[1], hsb[2] * 0.7f);
+        } else {
+            double min = Integer.MAX_VALUE;
+            Color result = color;
+            for (Color c : PRESETS) {
+                int dR = c.getRed() - color.getRed();
+                int dB = c.getBlue() - color.getBlue();
+                int dG = c.getGreen() - color.getGreen();
+
+                double diff = Math.sqrt(dR * dR + dG * dG + dB * dB);
+                if (diff < min) {
+                    min = diff;
+                    result = c;
+                }
+                if (min < 0.001) {
+                    break;
+                }
+            }
+            // System.out.println("from presets: " + result + ", min: " + min);
+            return result;
+        }
+    }
+
+    private static Color readColor(String line) {
+        String str;
+        if (line.length() > 6) {
+            str = line.trim();
+        } else {
+            str = line;
+        }
+        if (str.length() != 6) {
+            return null;
+        }
+        return new Color(Integer.parseInt(str.substring(0, 2), 16), Integer.parseInt(str.substring(2, 4), 16),
+                Integer.parseInt(str.substring(4, 6), 16));
+    }
+}
